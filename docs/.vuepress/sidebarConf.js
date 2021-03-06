37b90fe97a7af001b9adf7c03d70ed8f42b36878
Kidy <2013235002@qq.com>
2021-03-06 18:44:47 +0800

    ✨  feat(笔记): SpringBoot2 Web & Vert.x

--- docs/.vuepress/sidebarConf.js
+++ docs/.vuepress/sidebarConf.js
@@ -1,8 +1,9 @@
 const getDocPath = require('./getDocPath')
 module.exports = {
   '/docs/KChat': [getDocPath('KChat', false, '/docs/KChat/')],
-  '/study/Hive-note': [getDocPath('Hive学习笔记', false, '/study/Hive-note/')],
+  '/study/Hive': [getDocPath('Hive 学习笔记', false, '/study/Hive-note/')],
   '/study/SpringBoot2': [
-    getDocPath('SpringBoot2学习笔记', false, '/study/SpringBoot2-note/')
-  ]
+    getDocPath('SpringBoot2 学习笔记', false, '/study/SpringBoot2-note/')
+  ],
+  '/study/Vertx': [getDocPath('Vert.x 学习笔记', false, '/study/Vertx-note/')]
 }
